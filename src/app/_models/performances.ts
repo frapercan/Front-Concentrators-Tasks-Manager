cf6f195c1f8b332eb5fdb77f57fdc5ee0a8de442
Fran <franciscom.perez@babel.es>
2020-01-15 15:33:42 +0100

    performances

---
+++ src/app/_models/performances.ts
@@ -0,0 +1,7 @@
+export class Performance {
+    id_actuacion: number;
+    nombre: string;
+    descripcion: string;
+    grupo: number;
+    prioridad: number;
+}
---
+++ src/app/study/form/performances.component.ts
@@ -0,0 +1,80 @@
+﻿import { Component, Input, ViewChild, OnInit, OnDestroy } from "@angular/core";
+import {
+  FormBuilder, FormGroup, Validators
+
+} from "@angular/forms";
+import {
+  PackageService,
+  ConcentratorService,
+  StudyService
+} from "../../_services";
+import { Package, Issue } from "../../_models";
+import { SelectionModel } from "@angular/cdk/collections";
+import { MatTableDataSource, MatPaginator, MatSort } from "@angular/material";
+
+
+
+
+
+@Component({ selector: "performancesStep", templateUrl: "performances.component.html", styleUrls: ["../study-form.component.scss"] })
+export class PerformancesComponent implements OnInit {
+  @Input() public performancesFormGroup: FormGroup;
+  @Input() public performances: FormGroup;
+
+  dataSource;
+  displayedColumns: string[] = ["id_actuacion", "nombre", "descripcion", "selection"];
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  select = new SelectionModel<Performance>(true, []);
+  perfomancesSelection = PerformancesSelection;
+
+  constructor(
+    private studyService: StudyService
+
+  ) {
+  }
+
+  ngOnInit() {
+    this.loadAllPerformances();
+    this.performancesGroupValueChanged();
+
+
+
+  }
+
+  performancesGroupValueChanged() {
+    this.select.changed.subscribe(value =>
+      this.performancesFormGroup.setValue({
+        performances: value.source.selected,
+      })
+    );
+  }
+
+  private loadAllPerformances() {
+    this.studyService
+      .getPerformancesList()
+      .then(
+        performances => (
+          (this.dataSource = new MatTableDataSource(performances)),
+          (this.dataSource.sort = this.sort),
+          (this.dataSource.paginator = this.paginator)
+        )
+      );
+
+
+  }
+
+
+
+
+
+
+
+
+}
+
+export enum PerformancesSelection {
+  nothing = "",
+  act = 1
+}
